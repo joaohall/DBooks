b7cff7177cb7fb85388560b8b7c55d144c9d9a6c
joaohall <enderfirmite@gmail.com>
2023-11-07 02:09:37 -0300

    .

--- flowshop/front-end/src/components/navbar.tsx
+++ flowshop/front-end/src/components/navbar.tsx
@@ -4,18 +4,18 @@ import './navbar.css';
 const strings = ['','','',]
 
 export default function Navbar() {
-    return (
-    <nav className="navbar">
-      <div className="navbar-content">
-        <div>
-            <img src="" alt="Logo do site na barra de navegação" width={50} height={50}/>
+return (
+<nav className="navbar">
+    <div className="navbar-content">
+        <div className="navbar-links">
+            <a href="#">Sobre</a>
+            <a href="#">Produtos</a>
+            <a href="#">FAQ</a>
         </div>
         <div>
-            <a href="#">a</a>
-            <a href="#">a</a>
-            <a href="#">a</a>
+            <img src="" alt="Logo do site na barra de navegação" width={50} height={50} />
         </div>
-      </div>
-    </nav>
-  )
+    </div>
+</nav>
+)
 }
